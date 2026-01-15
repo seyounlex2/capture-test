25f98c8fb59d6e7d8869d199da76c0d7022c33a1
Lee Se Youn <sylee@sailingstone.io>
2026-01-15 10:07:33 +0900

    refactor: modify router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import CapturePage from "./pages/capture";
 import HomePage from "./pages/home";
 
 function App() {
   return (
-    <BrowserRouter basename="/capture-test">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/capture" element={<CapturePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
